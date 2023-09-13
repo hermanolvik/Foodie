4ca9cd1b0d20c991c9443412b2acd656486cf70d
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2023-09-13 20:11:16 +0200

    Update app.py

--- Back-End/app.py
+++ Back-End/app.py
@@ -1,15 +1,28 @@
-
 import openai
 
 # Initialize the OpenAI API
 openai.api_key = "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"
 
-input("Please write a nice recipe of the following ingredients: ")
-ingredients  = req
-# Text you want to send to the API
-prompt = "Please write a nice recipe of the following ingredients: "
+ingredients = []
+dietary_restrictions = ["none"]
 
+while True:
+    ingredient = input("Please write ingredients (enter 'done' to finish): ")
+    if ingredient.lower() == "done":
+        break
+    ingredients.append(ingredient)
 
+while True:
+    restriction = input("Please enter any dietary restrictions (enter 'done' to finish): ")
+    if restriction.lower() == "done":
+        break
+    dietary_restrictions.append(restriction)
+
+ingredient_String = ", ".join(ingredients)
+restriction_String = ", ".join(restriction)
+
+#T ext you want to send to the API
+prompt = f"Please write a nice recipe of the ingredients: {ingredient_String},restrict recipes to: {restriction_String}"
 
 # Make API request
 response = openai.Completion.create(
@@ -17,7 +30,3 @@ response = openai.Completion.create(
     prompt=prompt,
     max_tokens=1000
 )
-
-# Extract and print the generated text from the API response
-generated_text = response.choices[0].text.strip()
-print(f"Generated text: {generated_text}")
