cf81d77057d613a2995110057afbe380f1d96f70
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2023-09-14 18:28:22 +0200

    mvp för terminal

--- BackEnd/app.py
+++ BackEnd/app.py
@@ -27,6 +27,27 @@ def generate_recipe(json_object):
     generated_text = response.choices[0].text.strip()
     return generated_text
 
+### Minimal viable product (with terminal input) ###
+ingredients = []
+dietary_restrictions = ["none"]
+
+while True:
+    ingredient = input("Please write ingredients (enter 'done' to finish): ")
+    if ingredient.lower() == "done":
+        break
+    ingredients.append(ingredient)
+
+while True:
+    restriction = input("Please enter any dietary restrictions (enter 'done' to finish): ")
+    if restriction.lower() == "done":
+        break
+    dietary_restrictions.append(restriction)
+
+ingredient_String = ", ".join(ingredients)
+restriction_String = ", ".join(restriction)
+####
+
+
 # Example JSON object
 recipe_info = {
     "ingredients": ["bacon", "zucchini", "potato"],
