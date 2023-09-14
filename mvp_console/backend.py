9f73fdfc22b30761bc8312c7c773ad2e0a6ba978
olvik <olvik@net.chalmers.se>
2023-09-14 21:05:14 +0200

    Merge remote-tracking branch 'origin/website-openai-merge' into website-openai-merge

--- mvp_console/backend.py
+++ mvp_console/backend.py
@@ -22,5 +22,20 @@ while True:
 ingredient_String = ", ".join(ingredients)
 restriction_String = ", ".join(restriction)
 
+# Create the prompt for the API
+prompt = f"Please write a recipe that includes the following ingredients: {ingredients}."
+if dietary_restrictions != 'None':
+    prompt += f" The recipe should be suitable for someone with the following dietary restrictions: {dietary_restrictions}."
+prompt += " Recipe:"
+
+# Make API request
+response = openai.Completion.create(
+    engine="text-davinci-003",  # text-davinci-003 is the name of the latest engine based on GPT-3
+    prompt=prompt,
+    max_tokens=1000
+)
+
+# Extract and return the generated text from the API response
+generated_recipe = response.choices[0].text.strip()
 
 print(f"Generated Recipe:\n{generated_recipe}")
