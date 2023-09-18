7f4d601536a8672f759cffcd91b19cb7088637ce
Pontus Brylander <144108597+Brylander@users.noreply.github.com>
2023-09-18 08:29:20 +0200

    comment script.js
    commented the script.js

--- static/script.js
+++ static/script.js
@@ -1,8 +1,11 @@
 document.getElementById('search-button').addEventListener('click', function () {
     // Create the JSON data to send to the backend
+    //data will contain the string that was written in the search box on the first page
     const data = document.querySelector('.search-box').value;
+    //data is split into an array of strings, each string is the item separated by comma, it then trims the spaces on start and end
     const prompt = data.split(',').map(item => item.trim());
 
+    //This is the json structure to be sent to backend
     const jsonData = {
         ingredients: prompt
     };
@@ -15,13 +18,16 @@ document.getElementById('search-button').addEventListener('click', function () {
         },
         body: JSON.stringify(jsonData)
     })
-        .then(response => response.json())
-        .then(data => {
+        .then(response => response.json()) //First wait for response, after make response to json
+        .then(data => { //After it is made to json, use the data as a variable called "data"
             
-
+            //data is a json object and attributes can be found with dots
             let Title = data.title;
+            //<br> is html code for "\n"
+            //We replace the \n with <br>
             let Ingredients = data.ingredients.replace(/\n/g, '<br>');
             let Instructions = data.instructions.replace(/\n/g, '<br>');
+            //Sending the variables up to the app.py for the route of page_1 in flask
             location.href ="/page_1?title=" + Title + "&ingredients=" + Ingredients + "&instructions=" + Instructions;
             
             
