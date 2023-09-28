a330985d13376e6148012ebd30b8643489c038c1
Allan Gamal <63316423+AllanGamal@users.noreply.github.com>
2023-09-28 20:18:51 +0200

    Snyggare kod

--- static/base.js
+++ static/base.js
@@ -1,36 +1,25 @@
 
 const toggleSwitch = document.getElementById('toggleSwitch');
-const contentB = document.getElementById('Bbody');
-const contentH = document.getElementById('Hheader');
-const contentI = document.getElementById('IngredientsHead');
-const contentP = document.getElementById('portions-id');
-const contentA = document.getElementById('aboutId');
-
+const elements = [ // array av ojects
+    { id: 'Bbody', darkClass: 'dark-mode-body', lightClass: 'light-mode' },
+    { id: 'Hheader', darkClass: 'dark-mode-head', lightClass: 'light-mode' },
+    { id: 'IngredientsHead', darkClass: 'Ingredients-dark', lightClass: 'Ingredients-light' },
+    { id: 'portions-id', darkClass: 'dark-portions', lightClass: 'portions' },
+    { id: 'aboutId', darkClass: 'about-dark', lightClass: 'about-light' },
+];
 
 toggleSwitch.addEventListener('change', () => {
-    if (toggleSwitch.checked) {
-        if (contentB) contentB.classList.add('dark-mode-body');
-        if (contentH) contentH.classList.add('dark-mode-head');
-        if (contentP) contentP.classList.add('dark-portions');
-        if (contentI) contentI.classList.add('Ingredients-dark');
-        if (contentA) contentA.classList.add('about-dark');
-
-        if (contentH) contentH.classList.remove('light-mode');
-        if (contentB) contentB.classList.remove('light-mode');
-        if (contentI) contentI.classList.remove('Ingredients-light');
-        if (contentP) contentP.classList.remove('portions');
-        if (contentA) contentA.classList.remove('about-light');
-    } else {
-        if (contentB) contentB.classList.remove('dark-mode-body');
-        if (contentH) contentH.classList.remove('dark-mode-head');
-        if (contentP) contentP.classList.remove('dark-portions');
-        if (contentI) contentI.classList.remove('Ingredients-dark');
-        if (contentA) contentA.classList.remove('about-dark');
-
-        if (contentB) contentB.classList.add('light-mode');
-        if (contentH) contentH.classList.add('light-mode');
-        if (contentI) contentI.classList.add('Ingredients-light');
-        if (contentP) contentP.classList.add('portions');
-        if (contentA) contentA.classList.add('about-light');
-    }
+    elements.forEach(element => { // loopa igenom varje element i arrayn
+        const el = document.getElementById(element.id); 
+        if (!el) { // om elementet ej exitsterar på sidan, gå till nästa element
+            return;
+        }
+        if (toggleSwitch.checked) { 
+            el.classList.add(element.darkClass);
+            el.classList.remove(element.lightClass);
+        } else {
+            el.classList.add(element.lightClass);
+            el.classList.remove(element.darkClass);
+        }
+    });
 });
