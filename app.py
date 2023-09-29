d7e18f6ad5859a3e9e9b5171a3ec079bb732a66f
Nate <dobner@chalmers.se>
2023-09-29 17:16:46 +0200

    Fixed bug where search froze

--- app.py
+++ app.py
@@ -45,10 +45,6 @@ def generate_recipe(json_object):
     num_portions = json_object.get('number_of_portions', 4)
     measurement_unit = json_object.get('measurement_unit', 'metric (do not use cups, only metric units)')
 
-    print(dietary_restrictions)
-    print(num_portions)
-    print(measurement_unit)
-
     # Create the prompt for the API
     prompt = f"Please write a recipe that includes the following ingredients: {ingredients}."
     if dietary_restrictions != 'None':
